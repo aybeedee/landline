6865a2c5e488771e14c71325d523cc7f283e498b
AbdullahUmer575 <abdullahumer575@gmail.com>
2023-04-19 17:41:13 +0500

    first commit

---
+++ app/buyer/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>BUYER PAGE</h1>
+    </div>
+  )
+}
---
+++ app/seller/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+    return (
+      <div>
+        <h1>SELLER PAGE</h1>
+      </div>
+    )
+  }
+  
