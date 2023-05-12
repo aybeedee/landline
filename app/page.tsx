012428b0ab0b20f02bc80b873e2e76d2aa5f8ab5
AbdullahUmer575 <abdullahumer575@gmail.com>
2023-05-12 19:54:35 +0500

    fixed login validation

--- app/page.tsx
+++ app/page.tsx
@@ -10,26 +10,29 @@ export default function Home() {
           </div>
           <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
             <div className="card-body">
-                <div className="form-control">
-                <label className="label">
-                    <span className="label-text">Email</span>
-                </label>
-                <input type="email" placeholder="email" className="input input-bordered" />
-                </div>
-                <div className="form-control">
-                <label className="label">
-                    <span className="label-text">Password</span>
-                </label>
-                <input type="password" placeholder="password" className="input input-bordered" />
-                <label className="label">
-                    <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
-                </label>
-                </div>
-                <div className="form-control mt-6">
-                <button className="btn btn-primary">
-                    <Link href = '/user'>Login</Link>
-                </button>
-                </div>
+                <form action = '/user'>
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Email</span>
+                        </label>
+                        <input type="email" placeholder="default@example.com" className="input input-bordered" required/>
+                    </div>
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Password</span>
+                        </label>
+                        <input type="password" placeholder="password" className="input input-bordered" required/>
+                        <label className="label">
+                            <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                        </label>
+                    </div>
+                    <div className="form-control mt-6">
+                        <button type = "submit" className="btn btn-primary">
+                            {/* <Link href = '/user'>Login</Link> */}
+                            Login
+                        </button>
+                    </div>
+                </form>
             </div>
           </div>
       </div>
